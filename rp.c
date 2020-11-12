6393b54fa73c90eaf8d2b9a3f99e8b0e5251e413
AANDREWS001 <73622695+AANDREWS001@users.noreply.github.com>
2020-11-12 14:16:17 -0600

    Add files via upload
    Changes

--- rp.c
+++ rp.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2016-2020 Jeremy Iverson
+Copyright (c) 2016-2020 Jeremy Iverson & Ashton Andrews
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,275 +26,233 @@ THE SOFTWARE.
 /* fabs */
 #include <math.h>
 
+/* MPI API */
+#include <mpi.h>
+
 /* printf, fopen, fclose, fscanf, scanf */
 #include <stdio.h>
 
 /* EXIT_SUCCESS, malloc, calloc, free, qsort */
 #include <stdlib.h>
 
+#define MPI_SIZE_T MPI_UNSIGNED_LONG
+
 struct distance_metric {
   size_t viewer_id;
   double distance;
 };
 
+static int
+cmp(void const *ap, void const *bp)
+{
+  struct distance_metric const a = *(struct distance_metric*)ap;
+  struct distance_metric const b = *(struct distance_metric*)bp;
+
+  return a.distance < b.distance ? -1 : 1;
+}
+
 int
 main(int argc, char * argv[])
 {
-  size_t n, m;
+  int ret, p, rank;
+  size_t n, m, k;
+  double * rating;
+
+  /* Initialize MPI environment. */
+  ret = MPI_Init(&argc, &argv);
+  assert(MPI_SUCCESS == ret);
+
+  /* Get size of world communicator. */
+  ret = MPI_Comm_size(MPI_COMM_WORLD, &p);
+  assert(ret == MPI_SUCCESS);
+
+  /* Get my rank. */
+  ret = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  assert(ret == MPI_SUCCESS);
 
   /* Validate command line arguments. */
   assert(2 == argc);
 
-  /* ... */
-  char const * const fn = argv[1];
+  /* Read input --- only if your rank 0. */
+  if (0 == rank) {
+    /* ... */
+    char const * const fn = argv[1];
 
-  /* Validate input. */
-  assert(fn);
+    /* Validate input. */
+    assert(fn);
 
-  /* Open file. */
-  FILE * const fp = fopen(fn, "r");
-  assert(fp);
+    /* Open file. */
+    FILE * const fp = fopen(fn, "r");
+    assert(fp);
 
-  /* Read file. */
-  fscanf(fp, "%zu %zu", &n, &m);
+    /* Read file. */
+    fscanf(fp, "%zu %zu", &n, &m);
 
-  /* Allocate memory. */
-  double * const rating = malloc(n * m * sizeof(*rating));
+    /* Allocate memory. */
+    rating = malloc(n * m * sizeof(*rating));
 
-  /* Check for success. */
-  assert(rating);
+    /* Check for success. */
+    assert(rating);
 
-  for (size_t i = 0; i < n; i++) {
-    for (size_t j = 0; j < m; j++) {
-      fscanf(fp, "%lf", &rating[i * m + j]);
+    for (size_t i = 0; i < n; i++) {
+      for (size_t j = 0; j < m; j++) {
+        fscanf(fp, "%lf", &rating[i * m + j]);
+      }
     }
+
+    /* Close file. */
+    ret = fclose(fp);
+    assert(!ret);
   }
 
-  /* Close file. */
-  int const ret = fclose(fp);
-  assert(!ret);
+  /* Send number of viewers and movies to rest of processes. */
+  if (0 == rank) {
+    for (int r = 1; r < p; r++) {
+      ret = MPI_Send(&n, 1, MPI_SIZE_T, r, 0, MPI_COMM_WORLD);
+      assert(MPI_SUCCESS == ret);
+      ret = MPI_Send(&m, 1, MPI_SIZE_T, r, 0, MPI_COMM_WORLD);
+      assert(MPI_SUCCESS == ret);
+    }
+  } else {
+      ret = MPI_Recv(&n, 1, MPI_SIZE_T, 0, 0, MPI_COMM_WORLD,
+        MPI_STATUS_IGNORE);
+      assert(MPI_SUCCESS == ret);
+      ret = MPI_Recv(&m, 1, MPI_SIZE_T, 0, 0, MPI_COMM_WORLD,
+        MPI_STATUS_IGNORE);
+      assert(MPI_SUCCESS == ret);
+  }
+
+  /* Compute base number of viewers. */
+  size_t const base = 1 + ((n - 1) / p); // ceil(n / p)
+
+  /* Compute local number of viewers. */
+  size_t const ln = (rank + 1) * base > n ? n - rank * base : base;
+
+  /* Send viewer data to rest of processes. */
+  if (0 == rank) {
+    for (int r = 1; r < p; r++) {
+      size_t const rn = (r + 1) * base > n ? n - r * base : base;
+      ret = MPI_Send(rating + r * base * m, rn * m, MPI_DOUBLE, r, 0,
+        MPI_COMM_WORLD);
+      assert(MPI_SUCCESS == ret);
+    }
+  } else {
+    /* Allocate memory. */
+    rating = malloc(ln * m * sizeof(*rating));
+
+    /* Check for success. */
+    assert(rating);
+
+    ret = MPI_Recv(rating, ln * m, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD,
+      MPI_STATUS_IGNORE);
+    assert(MPI_SUCCESS == ret);
+  }
 
   /* Allocate more memory. */
-  double * const urating = malloc(m * sizeof(*urating));
+  double * const urating = malloc((m - 1) * sizeof(*urating));
 
   /* Check for success. */
   assert(urating);
 
-  /* Get user input. */
-  for (size_t j = 0; j < m - 1; j++) {
-    printf("Enter your rating for movie %zu: ", j + 1);
-    scanf("%lf", &urating[j]);
+  /* Get user input and send it to rest of processes. */
+  if (0 == rank) {
+    for (size_t j = 0; j < m - 1; j++) {
+      printf("Enter your rating for movie %zu: ", j + 1);
+      fflush(stdout);
+      scanf("%lf", &urating[j]);
+    }
+
+    for (int r = 1; r < p; r++) {
+      ret = MPI_Send(urating, m - 1, MPI_DOUBLE, r, 0, MPI_COMM_WORLD);
+      assert(MPI_SUCCESS == ret);
+    }
+  } else {
+    ret = MPI_Recv(urating, m - 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD,
+      MPI_STATUS_IGNORE);
+    assert(MPI_SUCCESS == ret);
   }
 
   /* Allocate more memory. */
-  //double prob[10] = { 0.0 };
-  double prob1[10] = { 0.0 };
-  double prob2[10] = { 0.0 };
-  double prob3[10] = { 0.0 };
-  double prob4[10] = { 0.0 };
+  struct distance_metric * const distance = calloc(n, sizeof(*distance));
 
+  /* Check for success. */
+  assert(distance);
 
-/* Compute probabilities */
-  /*for (int k = 0; k < 10; k++) {
-    for (size_t i = 0; i < n; i++) {
-      prob[k] += (rating[i * m + 4] == (k + 1) / 2.0);
-    }
-  }*/
-  
-  /* Compute probabilities Part 1 */
-  for (int k = 0; k < 10; k++) {
-    for (size_t i = 0; i < n; i++) {
-      //prob1[k] += (rating[i * m + 0] == (k + 1) / 2.0);
-    //prob1[k] = (rating[i * m + 0] == urating[0]);
+  /* Compute distances. */
 
-	if((rating[i * m + 0] == urating[0]) && (rating[i * m + 4] == (k + 1) / 2.0))
-	{
-		prob1[k] += (rating[i * m + 0] == (k + 1) / 2.0);
-		/*prob1[k] /= n;
-		printf("Movie 1's ratings: \n prob[%d] = %lf\n", k, prob1[k]);*/
-	}
-    }
-  }
-  
-  /* Compute probabilities Part 2 */
-  for (int k = 0; k < 10; k++) {
-    for (size_t i = 0; i < n; i++) {
-      //prob2[k] += (rating[i * m + 1] == (k + 1) / 2.0);
-    	//prob2[k] = (rating[i * m + 1] == urating[1]);
+    for (size_t i = 0; i<ln; i++){
+      distance[i].viewer_id = i;
+       for (size_t j = 0; j < m - 1; j++) {
+        distance[i].distance += fabs(urating[j] - rating[i * m + j]);
+        }
+       }
 
-	if((rating[i * m + 1] == urating[1]) && (rating[i * m + 4] == (k + 1) / 2.0))
-	{
-		prob2[k] += (rating[i * m + 1] == (k + 1) / 2.0);
-		/*prob2[k] /= n;
-		printf("Movie 2's ratings: \n prob[%d] = %lf\n", k, prob2[k]);*/
-	}
-	}
+  /* Every process except 0 sends their computation to rank 0 */
+if(rank > 0)
+{
+ret = MPI_Send(distance, ln, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+      assert(MPI_SUCCESS == ret);
+}
 
-  }
+/* Rank 0 iterates through all processors and receives each computation */
 
+else{
+     for(int r = 1; r<p; r++){
+	ret = MPI_Recv(distance + r * base * m, ln * m, MPI_DOUBLE, r, 0, MPI_COMM_WORLD,
+        MPI_STATUS_IGNORE);
+        assert(MPI_SUCCESS == ret);
+}
+}
 
-  /* Compute probabilities Part 3 */
-  for (int k = 0; k < 10; k++) {
-    for (size_t i = 0; i < n; i++) {
-      //prob3[k] += (rating[i * m + 2] == (k + 1) / 2.0);
-    	//prob3[k] = (rating[i * m + 2] == urating[2]);
-	
+ /* Only rank 0 is to compute the rest of the code */
 
-	if((rating[i * m + 2] == urating[2]) && (rating[i * m + 4] == (k + 1) / 2.0))
-	{
-		prob3[k] += (rating[i * m + 2] == (k + 1) / 2.0);
-		/*prob3[k] /= n;
-		printf("Movie 3's ratings: \nprob[%d] = %lf\n", k, prob3[k]);*/
-	}
-	}
+    if(rank == 0){
+
+  /* Sort distances. */
+  qsort(distance, n, sizeof(*distance), cmp);
+
+  /* Get user input. */
+  printf("Enter the number of similar viewers to report: ");
+  fflush(stdout);
+  scanf("%zu", &k);
+
+  /* Output k viewers who are least different from the user. */
+  printf("Viewer ID   Movie five   Distance\n");
+  printf("---------------------------------\n");
+
+  for (size_t i = 0; i < k; i++) {
+    printf("%9zu   %10.1lf   %8.1lf\n", distance[i].viewer_id + 1,
+      rating[distance[i].viewer_id * m + 4], distance[i].distance);
   }
 
-  
-  /* Compute probabilities Part 4 */
-  for (int k = 0; k < 10; k++) {
-    for (size_t i = 0; i < n; i++) {
-      //prob4[k] += (rating[i * m + 3] == (k + 1) / 2.0);
-    	//prob4[k] = (rating[i * m + 3] == urating[3]);
-	
+  printf("---------------------------------\n");
 
-	if((rating[i * m + 3] == urating[3]) && (rating[i * m + 4] == (k + 1) / 2.0))
-	{
- 		prob4[k] += (rating[i * m + 3] == (k + 1) / 2.0);
-		/*prob4[k] /= n;
-		printf("Movie 4's ratings: \nprob[%d] = %lf\n", k, prob4[k]);*/
-		
-	}
-		
-
-	}
-  }
-
- 
-  
-
-  /* Finalize computation of probabilities. */
-  for (int k = 0; k < 10; k++) {
-    //prob[k] /= n;
-    prob1[k] /= n;
-    prob2[k] /= n;
-    prob3[k] /= n;
-    prob4[k] /= n;
-  }
-printf("Movie 1's rating probabilities\n");
-  for (int k = 0; k < 10; k++) {
-    //printf("prob[%d] = %lf\n", k, prob[k]);
-    printf("prob[%d] = %lf\n", k, prob1[k]);
-  }
-printf("Movie 2's rating probabilities\n");
-  for (int k = 0; k < 10; k++) {
-    //printf("prob[%d] = %lf\n", k, prob[k]);
-    printf("prob[%d] = %lf\n", k, prob2[k]);
-  }
-printf("Movie 3's rating probabilities\n");
-  for (int k = 0; k < 10; k++) {
-    //printf("prob[%d] = %lf\n", k, prob[k]);
-    printf("prob[%d] = %lf\n", k, prob3[k]);
-
-  }
-printf("Movie 4's rating probabilities\n");
-  for (int k = 0; k < 10; k++) {
-    //printf("prob[%d] = %lf\n", k, prob[k]);
-    printf("prob[%d] = %lf\n", k, prob4[k]);
+  /* Compute the average to make the prediction. */
+  double sum = 0.0;
+  for (size_t i = 0; i < k; i++) {
+    sum += rating[distance[i].viewer_id * m + 4];
   }
 
   /* Output prediction. */
-<<<<<<< HEAD
-//double max, max1, max2, max3, max4, maxEquation;
-/*I have a feeling this isnt what exactly what you asked for
-for (int k = 0; k < 10; k++){
-for(size_t i=0; i<n; i++){
+  printf("The predicted rating for movie five is %.1lf.\n", sum / k);
 
-=======
-double max1, max2, max3, max4, maxEquation;
-for (int k = 0; k < 10; k++){
->>>>>>> 7c2c7237f2d27c9b4c6b4ffa519b9ab348043e43
-max1 = 0.0;
- if(prob1[k] > max1){
-    max1 = prob1[k];
 }
-}
-
-for (int k = 0; k < 10; k++){
-max2 = 0.0;
- if(prob2[k] > max2){
-    max2 = prob2[k];
-}
-}
-
-for (int k = 0; k < 10; k++){
-max3 = 0.0;
- if(prob3[k] > max3){
-    max3 = prob3[k];
-}
-}
-
-for (int k = 0; k < 10; k++){
-max4 = 0.0;
- if(prob4[k] > max4){
-    max4 = prob4[k];
-}
-}
-<<<<<<< HEAD
-=======
-
-if(max1 < max2){
-maxEquation = max2;
-}
-if(max1 < max2){
-maxEquation = max2;
-}
-if(max2 < max3){
-maxEquation = max3;
-}
-if(max3 < max4){
-maxEquation = max4;
-<<<<<<< HEAD
-}*/
-
-double max, max1, max2, max3, max4, maxEquation;
-
-for (int k = 0; k < 10;k++){
-for(size_t i = 0; i < n; i++){
-max1 = ((rating[i * m + 4]  == ((k+1) / 2.0)) * (rating[i * m + 0] == urating[0]) && (rating[i * m + 4] ==  (k+1) / 2.0));
-
-max2 = (rating[i * m + 1] == urating[1]) && (rating[i * m + 4] ==  (k+1) / 2.0);
-
-max3 = (rating[i * m + 2] == urating[2]) && (rating[i * m + 4] ==  (k+1) / 2.0);
-
-max4 = (rating[i * m + 3] == urating[3]) && (rating[i * m + 4] ==  (k+1) / 2.0);
-
-maxEquation = max1 * max2 * max3 * max4;
-
-if(maxEquation > max){
-max = maxEquation;
-}
-}
-}
-
-printf("The predicted rating for movie five is %.1lf.\n", max);
-=======
-}
-
-/*for (int k = 0; k<10;k++){
-for(size_t i = 0; i < n; i++){
-equation = ((rating[i * m + 4] == prob[k]) * (rating[i * m + 0] == urating[0] && rating[i * m + 4] == prob1[k]) * (rating[i * m + 1] == urating[1] && rating[i * m + 4] == prob2[k]) * (rating[i * m + 2] == urating[2] && rating[i * m + 4] == prob3[k]) * (rating[i * m + 3] == urating[3] && rating[i * m + 4] == prob4[k]));
-}
-}
-*/
-
-
-
   /* Deallocate memory. */
   free(rating);
   free(urating);
+  free(distance);
+
+  ret = MPI_Finalize();
+  assert(MPI_SUCCESS == ret);
 
   return EXIT_SUCCESS;
 }
-
+/* Code to put into terminal for testing
+ssh ssh.csbsju.edu
+ssh hpc5
+cd Desktop/CSCI351/CSCI-351-machine-learning/CSCI-351-machine-learning/
+mpicc -std=c99 -o rp rp.c
+mpiexec -n 8 ./rp ML-ratings-medium.txt
+*/
 
 
